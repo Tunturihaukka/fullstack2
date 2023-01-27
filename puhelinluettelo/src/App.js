adb64f21b3eccf76e1c2b6666057a01d0d2e1d2c
Aku <ap@Aku-MacBook-Air.local>
2023-01-27 16:10:19 +0200

    2.11

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,13 +1,22 @@
 import { useState } from 'react'
+import { useEffect} from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244'}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [filteredpersons, setFiltered] = useState(persons.concat()) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
   const [newFilter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+      setFiltered(response.data)
+    })
+  }, [])
 
   const addName = (adding) => {
     adding.preventDefault()
@@ -51,7 +60,7 @@ const App = () => {
     const filter = filterCheck()
     setFilter(filter)
     const initarray = []
-    if (filter != '') {
+    if (filter !== '') {
       const filtered = persons.reduce(
         (accumulator, currentPerson) => (
           checkFunction(accumulator, currentPerson, filter)
@@ -63,7 +72,6 @@ const App = () => {
       setFiltered(persons.concat())
     }
   } 
-
   return (
     <div>
       <h2>Phonebook</h2>
@@ -133,5 +141,7 @@ and is in the end returned. */
       )
     }
   }
+
+
 
 export default App
