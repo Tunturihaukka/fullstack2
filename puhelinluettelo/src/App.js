3debee673be1ca418a6b1b47ea6dee28e28bd066
Aku <ap@Aku-MacBook-Air.local>
2023-01-27 18:07:22 +0200

    2.14

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useEffect} from 'react'
-import axios from 'axios'
+import noteService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -10,8 +10,8 @@ const App = () => {
   const [newFilter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-    .get('http://localhost:3001/persons')
+    noteService
+    .getAll()
     .then(response => {
       setPersons(response.data)
       setFiltered(response.data)
@@ -29,17 +29,17 @@ const App = () => {
         name: nameholder,
         number: newNumber
       }
-      axios
-        .post('http://localhost:3001/persons', personObject)
+      noteService
+        .create(personObject)
         .then(response => {
-    })
-      setPersons(persons.concat(personObject))
-      if (!personObject.name.includes(newFilter) || newFilter === ''){
-        setFiltered(filteredpersons.concat(personObject))
-      }
+          setPersons(persons.concat(personObject))
+          if (!personObject.name.includes(newFilter) || newFilter === ''){
+            setFiltered(filteredpersons.concat(personObject))
+          }
+          setNewName('')
+          setNumber('')
+        })
     }
-    setNewName('')
-    setNumber('')
   }
 
   const nameHandler = (input) => {
@@ -76,14 +76,49 @@ const App = () => {
       setFiltered(persons.concat())
     }
   } 
+
+  const deletePerson = (person) => {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      noteService
+        .remove(person.id) 
+        .then(response => {
+          setPersons(persons.filter(p => p.name !== person.name).concat())
+          setFiltered(filteredpersons.filter(p => p.name !== person.name).concat())
+        })
+    }
+  }
+
+  const createFilterObject = () => {
+    const filterObject = {
+      filteredpersons : filteredpersons,
+      deletePerson : deletePerson
+    }
+    return (
+      filterObject
+    )
+  }
+
+  const createFormObject = () => {
+    const formObject = {
+      newName : newName,
+      newNumber : newNumber,
+      nameHandler : nameHandler,
+      numberHandler : numberHandler,
+      addName : addName
+    }
+    return (
+      formObject
+    )
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter newFilter={newFilter} filterHandler={filterHandler}/>
       <h2>add a new</h2>
-      <PersonForm newName={newName} newNumber={newNumber} nameHandler={nameHandler} numberHandler={numberHandler} addName={addName}/>
+      <PersonForm {...createFormObject()}/>
       <h2>Numbers</h2>
-      <Persons filteredpersons={filteredpersons}/>
+      <Persons {...createFilterObject()}/>
     </div>
   )
 
@@ -112,15 +147,22 @@ const PersonForm = ({newName, newNumber, nameHandler, numberHandler, addName}) =
   )
 }
 
-const Persons = ({filteredpersons}) => (
-  <ul>
-    {filteredpersons.map(person => 
-      <li key={person.name}>
-        {person.name} {person.number}
-      </li>
-    )}
-  </ul>
-)
+const Persons = ({filteredpersons, deletePerson}) => {
+  const handleClick = (person) => {
+    deletePerson(person)
+  }
+
+  return (
+      <ul>
+        {filteredpersons.map(person => 
+          <li key={person.name}>
+            {person.name} {person.number} &nbsp;
+            <button onClick={() => handleClick(person)}>delete</button>
+          </li>
+        )}
+      </ul>
+  )
+}
 
 const Filter = ({newFilter, filterHandler}) => (
   <div>
